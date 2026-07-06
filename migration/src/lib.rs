f7d066ff47a13719fe2da663c3db9a629b4ef548
MarcoAntonio22-wq <81771434+MarcoAntonio22-wq@users.noreply.github.com>
2026-07-06 17:56:29 -0300

    feat: implementação da migration de games e correção da migration do User

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,3 +3,4 @@ pub use sea_orm_migration::prelude::*;
 mod migrator;
 pub use migrator::Migrator;
 mod m20260706_193352_create_users;
+mod m20260706_195321_create_games;
--- migration/src/m20260706_195321_create_games.rs
+++ migration/src/m20260706_195321_create_games.rs
@@ -6,66 +6,79 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
 
         manager
             .create_table(
-                Table::create().table(User::Table).if_not_exists()
+                Table::create().table(Game::Table).if_not_exists()
+
                     .col(
-                        ColumnDef::new(User::Id)
+                        ColumnDef::new(Game::Id)
                         .uuid()
                         .not_null()
                         .primary_key()
-                    )
-                    .col(
-                        ColumnDef::new(User::Username)
-                        .string()
-                        .not_null()
                         .unique_key(),
                     )
                     .col(
-                        ColumnDef::new(User::Email)
+                        ColumnDef::new(Game::Title)
                         .string()
-                        .not_null()
-                        .unique_key(),
+                        .not_null(),
+
                     )
                     .col(
-                        ColumnDef::new(User::Password)
+                        ColumnDef::new(Game::Description)
+                        .text(),
+                    )
+                    .col(
+                        ColumnDef::new(Game::Developer)
                         .string()
                         .not_null(),
                     )
                     .col(
-                        ColumnDef::new(User::CreatedAt)
+                        ColumnDef::new(Game::Price)
+                        .decimal()
+                        .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Game::Genre)
+                        .string()
+                        .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(Game::ReleaseDate)
+                        .date()
+                        .null(),
+                    )
+                    .col(
+                        ColumnDef::new(Game::CreatedAt)
                         .timestamp_with_time_zone()
                         .not_null(),
                     )
                     .col(
-                        ColumnDef::new(User::UpdatedAt)
+                        ColumnDef::new(Game::UpdatedAt)
                         .timestamp_with_time_zone()
                         .not_null(),
                     )
-                    .to_owned(),  
+                    .to_owned(),
             )
             .await
-
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-
         manager
-            .drop_table(Table::drop().table(User::Table).to_owned())
+            .drop_table(Table::drop().table(Game::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum User {
+enum Game {
     Table,
     Id,
-    Username,
-    Email,
-    Password,
+    Title,
+    Description,
+    Developer,
+    ReleaseDate,
     CreatedAt,
-    UpdatedAt
+    UpdatedAt,
 }
